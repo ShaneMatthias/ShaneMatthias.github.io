9237044c88c8db2b8842b11e3d7f287625d0ead2
ShaneMatthias <shanematthiasloong@hotmail.com>
2018-10-15 16:23:49 -0400

    working on Gridding projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,16 +13,58 @@ export const Projects = () => {
 
             <div className='proj-retrieve'>
               <a href='https://github.com/ShaneMatthias/Retrieve'><img alt='Retrieve' src={require('../images/retrieve.png')} /></a>
-              <h1>
-                  Click for more info.
-              </h1>
+
+              <div className='proj-title'>
+                <h1>Retrieve</h1>
+              </div>
+
+              <div className='proj-desc'>
+                <h1>A Lost & Found application for iOS developed using React-Native. This iOS app allows users to create and view posts based on location. This is done using the Google Maps API.</h1>
+                <h1>Click on the thumbnail for more info.</h1>
+              </div>
+
+            </div>
+
+            <div className='proj-youtube'>
+              <a href='https://github.com/ShaneMatthias/Youtube-Browser'><img alt='Youtube' src={require('../images/youtube.png')} /></a>
+
+              <div className='proj-title'>
+                <h1>Youtube Browser</h1>
+              </div>
+
+              <div className='proj-desc'>
+                <h1>A youtube browser developed using React that has a similar layout to Youtube itself. This web app fetches data using the Youtube API.</h1>
+                <h1>Click on the thumbnail for more info.</h1>
+              </div>
+
+            </div>
+
+            <div className='proj-weather'>
+              <a href='https://github.com/ShaneMatthias/Weather-Forecast'><img alt='Weather' src={require('../images/weather.png')} /></a>
+
+              <div className='proj-title'>
+                <h1>Weather Forecast</h1>
+              </div>
+
+              <div className='proj-desc'>
+                <h1>A weather forecast web app developed using React-Redux.</h1>
+                <h1>Click on the thumbnail for more info.</h1>
+              </div>
+
             </div>
 
             <div className='proj-bookstore'>
-              <a href='https://github.com/ShaneMatthias/Retrieve'><img alt='Retrieve' src={require('../images/retrieve.png')} /></a>
-              <h1>
-                  Click for more info.
-              </h1>
+              <a href='https://github.com/ShaneMatthias/BookStoreManagement'><img alt='Bookstore' src={require('../images/bookstore.png')} /></a>
+
+              <div className='proj-title'>
+                <h1>Bookstore Management</h1>
+              </div>
+
+              <div className='proj-desc'>
+                <h1>A weather forecast web app developed using React-Redux.</h1>
+                <h1>Click on the thumbnail for more info.</h1>
+              </div>
+
             </div>
 
 
