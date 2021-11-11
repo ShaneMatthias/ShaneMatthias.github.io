ae6950fe201d0a41c8c38e9e44666685483e49dd
Shane Loong <matthias@Shanes-MacBook-Pro.local>
2021-11-11 15:20:00 -0500

    Updates

---
+++ precache-manifest.e98ca974ea9d53f977a4579916a66ccb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "3da10a4a63cad95cf176",
+    "url": "/static/css/main.5f1cf0ec.chunk.css"
+  },
+  {
+    "revision": "3da10a4a63cad95cf176",
+    "url": "/static/js/main.3da10a4a.chunk.js"
+  },
+  {
+    "revision": "ad1b474b8d8c66a07bec",
+    "url": "/static/css/1.943d2584.chunk.css"
+  },
+  {
+    "revision": "ad1b474b8d8c66a07bec",
+    "url": "/static/js/1.ad1b474b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6256c1bfe8deac8c40efad2b9c52bfde",
+    "url": "/static/media/cims.6256c1bf.png"
+  },
+  {
+    "revision": "c0976308fa3d6f409f9c475cbfc70629",
+    "url": "/static/media/bns.c0976308.png"
+  },
+  {
+    "revision": "42202866085a63db389bc75988b0092f",
+    "url": "/static/media/photo.42202866.png"
+  },
+  {
+    "revision": "a665ec6d8823371ca8562265f7441605",
+    "url": "/static/media/retrieve.a665ec6d.png"
+  },
+  {
+    "revision": "ad44f88da2ef832e85ede79723b5f719",
+    "url": "/static/media/youtube.ad44f88d.png"
+  },
+  {
+    "revision": "c2a157a250034183aa4aa94472e8a6c9",
+    "url": "/static/media/weather.c2a157a2.png"
+  },
+  {
+    "revision": "86a4ff232b20c12efe7aca886de0aae8",
+    "url": "/static/media/bookstore.86a4ff23.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "82e31c5da3a9e372a8f592fb01d5e666",
+    "url": "/static/media/redux.82e31c5d.png"
+  },
+  {
+    "revision": "762edb84b0bf808c1746d10188ebf04a",
+    "url": "/static/media/graphql.762edb84.png"
+  },
+  {
+    "revision": "85458a6717bed07d7db5146928d3cdd0",
+    "url": "/static/media/nodejs.85458a67.png"
+  },
+  {
+    "revision": "3851cc047b9284b97c521cc1a7d8a512",
+    "url": "/static/media/mongodb.3851cc04.png"
+  },
+  {
+    "revision": "d116e5c6a9e6bd4ac814ffde9a8b2125",
+    "url": "/static/media/psql.d116e5c6.png"
+  },
+  {
+    "revision": "faae11a6b87547339411b40ac228d4d7",
+    "url": "/static/media/css3.faae11a6.png"
+  },
+  {
+    "revision": "614e81bd92afc62be8127beac5e1b9af",
+    "url": "/static/media/c++.614e81bd.png"
+  },
+  {
+    "revision": "26603f1898814308dced061cf1895cc2",
+    "url": "/static/media/c.26603f18.png"
+  },
+  {
+    "revision": "3420e571b3d7a4a348d8fad91e3bfda4",
+    "url": "/static/media/jquery.3420e571.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "0d246dd9746face2af6b6380c948bd09",
+    "url": "/static/media/background.0d246dd9.png"
+  },
+  {
+    "revision": "1c89f29981ea9a29de1976b52431a137",
+    "url": "/index.html"
+  }
+];
